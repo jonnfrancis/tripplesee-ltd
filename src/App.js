61a7923aae0a0f5b459295addc2f15587150fa49
John Francis <johnmosima7@gmail.com>
2022-03-16 13:34:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <h1 className="btn">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    // screens: {
+    //   sm: '600px'
+    // },
+    // colors:{
+    //   indigo: {
+    //     light: '#b3bcf5',
+    //     DEFAULT: '5c6ac4',
+    //     dark: '#202e78' 
+    //   },
+    // },
+    // spacing:{
+    //   1: '0.35rem'
+    // },
+    extend: {
+      // screens: {
+      //   'xl': '1600px'
+      // },
+    },
+  },
+  plugins: [],
+}
